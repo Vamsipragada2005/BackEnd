e999d91c1ea7527bee62eb77fb8bbc1da90d5845
PRAGADA VAMSI <vamsipragada1750@gmail.com>
2025-12-05 11:14:58 +0530

    Add initial server setup and package configuration

---
+++ mybackend/index.js
@@ -0,0 +1,8 @@
+import http from 'http';
+const server = http.createServer((req,res) =>{
+        res.writeHead(200,{"Content-Type":"text/plain"});
+        res.end("This is from the backend");
+});
+server.listen(7007,()=>{
+    console.log('server running at port ${7007}');
+})
